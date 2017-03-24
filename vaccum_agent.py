15acfa8cc695d1beb1be936cded80faeda638196
PeacefulAlien <sun.zao@me.com>
2017-03-24 16:54:00 +0100

    class_agent modification commit

--- vaccum_agent.py
+++ vaccum_agent.py
@@ -12,6 +12,8 @@ import random
 WIDTH = 3
 HEIGTH = 4
 
+X_START = random.randint(0, WIDTH-1)
+Y_START = random.randint(0, HEIGTH-1)
 
 class environment:
 
@@ -28,10 +30,55 @@ class environment:
 			self.matrix_map[self.x_map][self.y_map] = 1
 		return self.matrix_map
 
+class agent():
+	
+ 	def __init__(self):
+		self.x_agent = X_START
+		self.y_agent = Y_START
+		
+	def agent_move_left(self):
+		if x_agent - 1 >= 0:
+			x_agent = x_agent - 1
+		else:
+			x_agent = x_agent
+		return x_agent
+	
+	def agent_move_right(self):
+		if x_agent + 1 <= 2:
+			x_agent = x_agent + 1
+		else:
+			x_agent = x_agent
+		return x_agent
+	
+	def agent_move_up(self):
+		if y_agent - 1 >= 0:
+			y_agent = y_agent - 1
+		else:
+			y_agent = y_agent
+		return y_agent
+		
+	def agent_move_down(self):
+		if y_agent + 1 <= 3:
+			y_agent = y_agent + 1
+		else:
+			y_agent = y_agent
+		return y_agent
+	
+	def agent_clean(self, status):
+		if status == 1:
+			print('''
+				The current square is dirty.
+				Vaccum agent is working on it...
+				''')
+			status = 0
+		return status
+	
+	
 def main():
 	environment_01 = environment()
 	print(environment_01.matrix_map)
 	
 	environment_01.dirt_generator()
 	print(environment_01.matrix_map)
+
 	
