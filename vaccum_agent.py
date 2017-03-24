a90c535472c31cf8da95648699b2761673b31aa3
PeacefulAlien <sun.zao@me.com>
2017-03-24 07:48:55 +0100

    class_map finished, class_agent initial

--- vaccum_agent.py
+++ vaccum_agent.py
@@ -1,5 +1,8 @@
 """
-
+	this practice aiming at build a environment and its vaccum cleaner agent
+	the environment size can be pre-defined, and it is a mahattan map, each
+	square has a chance to get dirty. the agent is designed to clean up the 
+	dirty squares. 
 """
 
 
@@ -15,16 +18,17 @@ class environment:
 	def __init__(self):
 		self.x_map = 0
 		self.y_map = 0
-		self.matrix_map = [[0 for i in range(WIDTH)] for j in range(HEIGTH)]
+		self.matrix_map = [[0 for i in range(HEIGTH)] for j in range(WIDTH)]
 		
 	def dirt_generator(self):
 		for i in range(WIDTH * HEIGTH):
 			self.x_map = random.randint(0, WIDTH-1)
 			self.y_map = random.randint(0, HEIGTH-1)
+			print("{},{}".format(self.x_map, self.y_map))
 			self.matrix_map[self.x_map][self.y_map] = 1
 		return self.matrix_map
 
-if __name__ == '__main__':
+def main():
 	environment_01 = environment()
 	print(environment_01.matrix_map)
 	
